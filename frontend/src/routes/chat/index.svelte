1db1079918969ae827ffd65d2dba817b04c8902e
vagueLima <o.lucas.lima@gmail.com>
2022-08-13 18:44:01 -0300

    login and sign up with redirect to chat

---
+++ frontend/src/routes/chat/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { user } from '../../stores/index';
+	if ($user.authenticated) {
+		console.log('you can stay here');
+	} else {
+		console.log('you cant stay here');
+		goto('/login')
+
+	}
+</script>
+
+<h1>Welcome to the chat</h1>>
