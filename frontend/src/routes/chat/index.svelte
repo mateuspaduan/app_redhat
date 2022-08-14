d3e4dcfe6acb0ddc0e71d2524c1f275936baa6b8
vagueLima <o.lucas.lima@gmail.com>
2022-08-14 12:41:06 -0300

    fixed route redirect on chat

--- frontend/src/routes/chat/index.svelte
+++ frontend/src/routes/chat/index.svelte
@@ -1,13 +1,16 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { user } from '../../stores/index';
-	if ($user.authenticated) {
-		console.log('you can stay here');
-	} else {
-		console.log('you cant stay here');
-		goto('/login')
+	import { onMount } from 'svelte';
 
-	}
+	onMount(async () => {
+		if ($user.authenticated) {
+			console.log('you can stay here');
+		} else {
+			console.log('you cant stay here');
+			goto('/login', { replaceState: true });
+		}
+	});
 </script>
 
 <h1>Welcome to the chat</h1>>
