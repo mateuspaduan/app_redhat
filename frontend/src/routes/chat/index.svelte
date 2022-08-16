03ef226d28e985dfb06b7096037318287f9eadd8
vagueLima <o.lucas.lima@gmail.com>
2022-08-15 21:06:31 -0300

    figure out chat css and automatic scroll down

--- frontend/src/routes/chat/index.svelte
+++ frontend/src/routes/chat/index.svelte
@@ -1,17 +1,31 @@
 <style>
-	.footerChat{
+	.footerChat {
 		display: grid;
 		grid-template-columns: 4fr 1fr;
-		grid-column-gap: 1rem;
-		padding: 1rem;
+		padding: 2rem;
+		grid-gap: 1rem;
+	}
+
+	.mainChat {
+		height: 100%;
+		flex-grow: 20;
+		flex-shrink: 0;
+		overflow-y: scroll;
+	}
+	.chat {
+		height: 100%;
+		display: grid;
+		grid-template-rows: 12fr 1fr;
 	}
 </style>
 
 <script>
 	import { goto } from '$app/navigation';
-	import { user } from '../../stores/index';
-	import { onMount } from 'svelte';
-	import Message from './message.svelte'
+	import { user, chat } from '../../stores/index';
+	import { onMount, afterUpdate, tick } from 'svelte';
+	import Message from './message.svelte';
+
+	let chatDiv;
 	onMount(async () => {
 		if ($user.authenticated) {
 			console.log('you can stay here');
@@ -20,18 +34,46 @@
 			//goto('/login', { replaceState: true });
 		}
 	});
+
+	async function sendMessage() {
+		$chat.messages.push({ username: $user.username, text: $chat.currentMessage });
+		$chat.currentMessage = '';
+	}
+
+	afterUpdate(() => {
+		console.log('afterUpdate');
+		if ($chat.messages.length > 0) scrollToBottom(chatDiv);
+	});
+
+	$: if ($chat.messages.length > 0 && chatDiv) {
+		console.log('tick');
+		scrollToBottom(chatDiv);
+	}
+
+	const scrollToBottom = async (node) => {
+		node.scroll({ top: node.scrollHeight, behavior: 'smooth' });
+	};
 </script>
 
 <div class="chat">
-	<div class="mainChat"> 
-		<Message username="lucas" message="rooooi?"></Message>
-		<Message username="mateus" message="e ai tudo bem?"></Message>
-
+	<div class="mainChat" bind:this="{chatDiv}">
+		{#each $chat.messages as { username, text }}
+			<Message username="{username}" message="{text}" />
+		{/each}
 	</div>
 	<div class="footerChat">
-		<input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Type your message here">
-		<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+		<input
+			bind:value="{$chat.currentMessage}"
+			class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+			id="inline-full-name"
+			type="text"
+			placeholder="Type your message here"
+		/>
+		<button
+			on:click="{sendMessage}"
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		>
 			Send
-		  </button>
+		</button>
 	</div>
 </div>
--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -5,5 +5,10 @@ export const isDev = writable(process.env.NODE_ENV === 'development');
 export const user = writable({
     username: "lucas",
     password: "",
-    authenticated: false
+    authenticated: false,
+  })
+
+  export const chat = writable({
+    messages: [{username:"lucas", text:"hello"}, {username:"mateus", text:"heeey"}],
+    currentMessage: ""
   })
